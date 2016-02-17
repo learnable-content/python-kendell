e61483dafbd1032dc2a6e67a27f44a15b1340aa5
kbyrdsokker33 <kbyrdsokker33@gmail.com>
2016-02-16 22:08:51 -0500

    Update search.py

--- search.py
+++ search.py
@@ -1,33 +1,20 @@
-#Linear Search
-def linear(list,target):
-  for i in range(len(list)):
-      if list[i]==target:
-         return list[i]
-  
-  return -1 #(or print an error that says "item not found in list")
+#Linear and Binary Search
+num_list=[3,2,8,12,11,15,22,17,25,55,33]
+target=100
+def linearsearch(list,target):
+    for i in range(len(list)):
+        if list[i]==target:
+            return list[i]
+            
+    return -1 #return "Couldn't find value"
+    
+result=linearsearch(num_list,target)
+print(result)
 
-result1=linear([1,5,8.2,3,16,22,19,58,44,37], 22)
-result2=linear([1,5,8.2,3,16,22,19,58,44,37], 45)
-result3=linear (["soccer","basketball", "football", "baseball", "hockey", "rugby", "frisbee"], "baseball")
-print(result1+"\n")
-print(result2+"\n")
+#Best Case Run Time-O(1)
+#length of list--n
+#Worst/Average Case--O(n)
 
-#Binary Search
-def binary(list, target):
-   start=0
-   end=len(list)-1
-   while start<end:
-     mid=(start+end)/2
-     if list[mid]==target:
-        return target
-     elif list[mid]>target:
-        end=mid-1
-     else:
-        start=mid+1
-   return -1
-        
-result4=binary([ 1, 7, 44, 45, 46, 58, 67, 99, 145], 67)
-print(result4+"\n")
   
 
 
